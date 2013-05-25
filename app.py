d730b78a0fb1e6008a46a56be36c6ea10ad32d58
Sebastian Reimers <sebastian.reimers@googlemail.com>
2013-05-25 15:04:11 +0000

    remove debug var

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def alsa(idx=0):
     return render_template('alsa.html', mixers=mixers, volumes=volumes)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
