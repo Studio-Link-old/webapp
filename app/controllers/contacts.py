08f5044adc51b0a62638ae3263362c70066d3d30
Sebastian Reimers <sreimers@aoit.de>
2013-07-01 00:51:06 +0200

    add first contacts mockup

--- app/controllers/contacts.py
+++ app/controllers/contacts.py
@@ -7,4 +7,12 @@ mod = Blueprint('contacts', __name__, url_prefix='/contacts')
 
 @mod.route('/')
 def index():
-    return render_template("contacts/index.html")
+    contacts = Contact.query.all()
+    return render_template("contacts/index.html", contacts=contacts)
+
+@mod.route('/add/')
+def add():
+    contact = Contact('test2', 'fe80::21e:68ff:fed5:333')
+    db.session.add(contact)
+    db.session.commit()
+    return render_template("contacts/add.html")
