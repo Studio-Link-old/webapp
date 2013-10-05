9dad409640ba6136b6e08a2cf757f06cacbf6fee
Sebastian Reimers <sreimers@aoit.de>
2013-10-05 17:16:14 +0200

    fix device bootstrap (empty database) bug

--- app/tasks.py
+++ app/tasks.py
@@ -9,16 +9,18 @@ import redis
 import urllib3
 import alsaaudio
 
-http = urllib3.PoolManager()
-store = redis.Redis('127.0.0.1')
-settings = Settings.query.get(1)
-devices = alsaaudio.cards()
-idx = devices.index(settings.device)
-device = 'hw:' + str(idx)
 
+def device_init():
+    http = urllib3.PoolManager()
+    store = redis.Redis('127.0.0.1')
+    settings = Settings.query.get(1)
+    devices = alsaaudio.cards()
+    idx = devices.index(settings.device)
+    device = 'hw:' + str(idx)
 
 @celery.task
 def rtp_tx():
+    device_init()
     transmitter = RTPtransmitter(audio_device=device, ipv6=True,
                                  receiver_address='::1')
     transmitter.run()
@@ -30,6 +32,7 @@ def rtp_tx():
 
 @celery.task
 def rtp_rx():
+    device_init()
     caps = "application/x-rtp,media=(string)audio,clock-rate=(int)48000,encoding-name=(string)X-GST-OPUS-DRAFT-SPITTKA-00"
     receiver = RTPreceiver(caps=caps, audio_device=device, ipv6=True)
     receiver.run()
@@ -41,6 +44,7 @@ def rtp_rx():
 
 @celery.task
 def play_audio():
+    device_init()
     store.setex('lock_play_audio', 'true', '30')
     player = Play(audio_device=device)
     player.run()
@@ -51,11 +55,13 @@ def play_audio():
 
 @celery.task
 def api_peer_status(host):
+    device_init()
     r = http.request('GET', 'http://['+host+']/api1/peer_status')
     return True
 
 
 @celery.task
 def api_peer_invite(host):
+    device_init()
     r = http.request('GET', 'http://['+host+']/api1/peer_status')
     return True
