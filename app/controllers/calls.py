0b7bded22bb083fe6924218922366ac914a3d317
Sebastian Reimers <service@it-sreimers.de>
2014-03-15 11:12:23 +0100

    refs #13 - rewrite dial action/form

--- app/controllers/calls.py
+++ app/controllers/calls.py
@@ -12,29 +12,65 @@
 from flask import Blueprint, render_template, redirect, request, url_for, flash
 from app import db
 from app.models.accounts import Accounts
+from app.forms.dial import DialForm
 import time
 import json
 import requests
 import urllib3
+import redis
 
-http_small = urllib3.PoolManager(timeout=1)
 mod = Blueprint('calls', __name__, url_prefix='/calls')
+http_small = urllib3.PoolManager(timeout=1)
+store = redis.Redis('127.0.0.1')
 
 
-@mod.route('/')
+@mod.route('/', methods=('GET', 'POST'))
 def index():
-    accounts = Accounts.query.all()
+    form = DialForm(request.form)
     ipv6 = ""
     try:
         ipv6 = http_small.request('GET', 'http://ipv6.studio-connect.de/').data
     except:
         pass
-    return render_template('calls/index.html', accounts=accounts,ipv6=ipv6)
+
+    db_accounts = Accounts.query.all()
+    accounts = []
+    if ipv6:
+        accounts.append(('Local IPv6', 'Local IPv6'))
+
+    for account in db_accounts:
+        accounts.append((account.name, account.name))
+    form.accounts.choices = accounts
+
+    if form.validate_on_submit():
+        try:
+            r = requests.get('http://127.0.0.1:8000/?d')  # List active calls
+        except:
+            pass
+        store.set('call_number', form.number.data)
+        store.set('call_account', form.accounts.data)
+        return redirect('/calls/dial')
+    if form.errors:
+        for error in form.errors:
+            flash(error+': '+form.errors[error][0], 'danger')
+
+    if not accounts and not ipv6:
+        return render_template('calls/no_accounts.html')
+    return render_template('calls/index.html', form=form, ipv6=ipv6)
 
 
 @mod.route('/dial')
 def dial():
-    return render_template('calls/index.html')
+    r = ""
+    try:
+        r = requests.get('http://127.0.0.1:8000/?l')  # List active calls
+    except:
+        pass
+    return render_template('calls/dial.html',
+                           call_number=store.get('call_number'),
+                           call_account=store.get('call_account'),
+                           baresip=r
+                           )
 
 
 @mod.route('/events')
---
+++ app/forms/dial.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +--------------------------------------------------------------------------+
+# |   _____ __            ___          ______                            __  |
+# |  / ___// /___  ______/ (_)___     / ____/___  ____  ____  ___  _____/ /_ |
+# |  \__ \/ __/ / / / __  / / __ \   / /   / __ \/ __ \/ __ \/ _ \/ ___/ __/ |
+# | ___/ / /_/ /_/ / /_/ / / /_/ /  / /___/ /_/ / / / / / / /  __/ /__/ /_   |
+# |/____/\__/\__,_/\__,_/_/\____/   \____/\____/_/ /_/_/ /_/\___/\___/\__/   |
+# |Copyright Sebastian Reimers 2013 - 2014 studio-connect.de                 |
+# |License: BSD-2-Clause (see LICENSE File)                                  |
+# +--------------------------------------------------------------------------+
+
+from flask.ext.wtf import Form
+from wtforms import TextField, SelectField
+from wtforms.validators import Required, ValidationError
+
+DEFAULT_CHOICES = []
+
+
+class DialForm(Form):
+    number = TextField('', [Required()])
+    accounts = SelectField('', choices=DEFAULT_CHOICES)
