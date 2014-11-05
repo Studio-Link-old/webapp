3656d6f905364c3881218ea6df3918dc949fc1de
Sebastian Reimers <sebastian.reimers@gmail.com>
2014-11-05 22:42:11 +0100

    add recording blueprint and fix gevent requirement

--- app/__init__.py
+++ app/__init__.py
@@ -49,6 +49,9 @@ app.register_blueprint(settingsModule)
 from app.controllers.routing import mod as routingModule
 app.register_blueprint(routingModule)
 
+from app.controllers.recording import mod as recordingModule
+app.register_blueprint(recordingModule)
+
 @app.before_request
 def before_request():
     if store.get('oncall') == 'true':
---
+++ app/controllers/recording.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +--------------------------------------------------------------------------+
+# |   _____ __            ___          __    _       __                      |
+# |  / ___// /___  ______/ (_)___     / /   (_)___  / /__                    |
+# |  \__ \/ __/ / / / __  / / __ \   / /   / / __ \/ //_/                    |
+# | ___/ / /_/ /_/ / /_/ / / /_/ /  / /___/ / / / / ,<                       |
+# |/____/\__/\__,_/\__,_/_/\____/  /_____/_/_/ /_/_/|_|                      |
+# |Copyright Sebastian Reimers 2013 - 2014 studio-link.de                    |
+# |License: BSD-2-Clause (see LICENSE File)                                  |
+# +--------------------------------------------------------------------------+
+
+from flask import Blueprint, request, flash, url_for, redirect, Response
+from flask import render_template
+
+mod = Blueprint('recording', __name__, url_prefix='/recording')
+
+
+@mod.route('/index')
+def index():
+    return render_template('recording.html')
