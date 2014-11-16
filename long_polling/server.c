0dd4d633fcb43739b3a98b7178b16f552a1d1f96
Sebastian Reimers <sebastian.reimers@gmail.com>
2014-11-16 16:32:38 +0100

    add long_polling native c server

---
+++ long_polling/server.c
@@ -0,0 +1,88 @@
+#include <sys/types.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <microhttpd.h>
+#include <stdio.h>
+#include <hiredis/hiredis.h>
+
+#define PORT 8888
+
+
+	static int
+answer_to_connection (void *cls, struct MHD_Connection *connection,
+		const char *url, const char *method,
+		const char *version, const char *upload_data,
+		size_t *upload_data_size, void **con_cls)
+{
+	const char *page = "{}";
+	struct MHD_Response *response;
+	int ret;
+	int j;
+	redisContext *context;
+	redisReply *reply;
+	struct timeval timeout = { 29, 0 }; // 29 seconds
+	context = redisConnect("127.0.0.1", 6379);
+
+
+	redisSetTimeout(context, timeout);
+
+	reply = redisCommand(context, "GET baresip_status");
+	printf("baresip_status) '%s'\n", reply->str);
+	if (strcmp(reply->str, "INCOMING") == 0) {
+		page = "{\"INCOMING\": \"sip:studio@studio.lan\"}\n";
+		freeReplyObject(reply);
+		goto out;
+	}
+	freeReplyObject(reply);
+
+	reply = redisCommand(context,"SUBSCRIBE baresip_call_event");
+	printf("subscribe_status) '%s'\n", reply->str);
+	freeReplyObject(reply);
+	while(redisGetReply(context, &reply) == REDIS_OK) {
+		// consume message
+		if (reply->type == REDIS_REPLY_ARRAY) {
+			for (j = 0; j < reply->elements; j++) {
+				printf("%u) %s\n", j, reply->element[j]->str);
+			}
+			if (strcmp(reply->element[2]->str, "INCOMING") == 0) {
+				page = "{\"INCOMING\": \"sip:studio@studio.lan\"}\n";
+				freeReplyObject(reply);
+				goto out;
+			}
+		}
+		freeReplyObject(reply);
+	}
+
+out:
+	response =
+		MHD_create_response_from_buffer (strlen (page), (void *) page, 
+				MHD_RESPMEM_PERSISTENT);
+	ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
+
+
+	MHD_destroy_response (response);
+	redisFree(context);
+
+	return ret;
+}
+
+
+	int
+main ()
+{
+	struct MHD_Daemon *daemon;
+
+	daemon = MHD_start_daemon (MHD_USE_THREAD_PER_CONNECTION, PORT, NULL, NULL,
+			&answer_to_connection, NULL, MHD_OPTION_END);
+	if (NULL == daemon)
+		return 1;
+
+	while(1==1) {
+		sleep(1);
+	}
+
+	MHD_stop_daemon (daemon);
+
+	return 0;
+}
