bf7016cbfa43f95d2ec16b9a31783431238bd332
Sebastian Reimers <sebastian.reimers@gmail.com>
2014-11-22 12:49:17 +0100

    fix py-jack requirement and add first routing version

--- app/controllers/routing.py
+++ app/controllers/routing.py
@@ -11,10 +11,29 @@
 
 from flask import Blueprint, request, flash, url_for, redirect, Response
 from flask import render_template
+import jack
 
 mod = Blueprint('routing', __name__, url_prefix='/routing')
 
 
 @mod.route('/index')
 def index():
-    return render_template('routing.html')
+    try:
+        ports = jack.get_ports()
+    except jack.NotConnectedError:
+        jack.attach("studio-webapp")
+        ports = jack.get_ports()
+
+    inports = []
+    outports = []
+    connects = {}
+
+    for port in ports:
+        if (jack.get_port_flags(port) & jack.IsInput) > 0:
+            inports.append(port)
+            connects[port] = jack.get_connections(port)
+        if (jack.get_port_flags(port) & jack.IsOutput) > 0:
+            outports.append(port)
+
+
+    return render_template('routing.html', inports=inports, outports=outports, connects=connects)
